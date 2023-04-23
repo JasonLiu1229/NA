236cb4b194cabfa325ba31899544a58a03f26f0f
JasonLiu1229 <100850638+JasonLiu1229@users.noreply.github.com>
2023-04-23 18:50:46 +0200

    oefening a af

--- Population_growth.py
+++ Population_growth.py
@@ -3,12 +3,13 @@ import math
 
 class population_growth:
     def __init__(self, in_t) -> None:
-        self.N0 = 100000
+        self.N0 = 1000000
         self.v = 35000
         self.t = in_t
         self.lm = 1
 
-    def function_cal(self):
+    def function_cal(self, in_lm: float or int = 1):
+        self.lm = in_lm
         return self.N0 * pow(math.e, self.t * self.lm) + self.v * (
                 (pow(math.e, self.t * self.lm) - 1) / self.lm)
 
@@ -35,13 +36,38 @@ class population_growth:
                 continue
             if not right_bracket and current_val >= end:
                 setting = False
-
-            self.lm = current_val
-            print("lambda = " + str(self.lm) + ", output = " + str(self.function_cal()))
-            calculated_vals.append(self.function_cal())
+            print("lambda = " + str(current_val) + ", output = " + str(self.function_cal(current_val)))
+            calculated_vals.append(self.function_cal(current_val))
             input_vals.append(current_val)
             current_val += interval
 
             if current_val > end:
                 setting = False
         return calculated_vals, input_vals
+
+    def derivative(self, in_lm: float or int = 0):
+        self.lm = in_lm
+        derivative = ((self.N0 * self.t * pow(self.lm, 2) + self.t * self.v * self.lm + self.v) *
+                      pow(math.e, self.lm * self.t) + self.v) / pow(self.lm, 2)
+        return derivative
+
+    def Newton(self, start_value: int or float = 0, end_value: int or float = 0, iterations: int = 1000) -> int:
+        """
+        Method of Newton on the population growth:
+        Method of Newton = x1 = x0 - F(x0) / F'(x0)
+        :param start_value: value of x0
+        :param end_value: the value we want to approach
+        :param iterations: How accurate we want it to be
+        :return: lambda --> self.lm
+        """
+        cal_val = self.function_cal(start_value)
+        cal_x = start_value
+        out_x = cal_x
+        for i in range(iterations):
+            if cal_val > end_value:
+                out_x = cal_x
+            else:
+                return out_x
+
+            cal_x = cal_x - (self.function_cal(cal_x) / self.derivative(cal_x))
+            cal_val = self.function_cal(cal_x)
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
     # Verkrijg een grafiek van N(1) als functie van λ ∈ (0, 1]
     pop = population_growth(1)
-    y, x = pop.calculate(0, 1, False, interval=0.1)
+    y, x = pop.calculate(0, 1, False, interval=0.01)
 
     plt.plot(x, y)
     plt.xlabel('x - axis')
@@ -13,3 +13,5 @@ if __name__ == '__main__':
     plt.title('Population growth')
     plt.show()
 
+    # b startwaarde = 0.21
+    print(pop.Newton(0.21, 1264000))
